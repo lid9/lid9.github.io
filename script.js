3478facfe472d593eb7eb9541ca6a2dc8144f8a7
lid9 <91291679+lid9@users.noreply.github.com>
2023-09-30 11:31:43 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  document.getElementById("text").innerHTML = "efakjlkwefhej'lhgw;a";
+}
+
+function myOtherFunction() {
+  document.getElementById("myImage").src = "https://cdn-icons-png.flaticon.com/256/616/616720.png";
+}
